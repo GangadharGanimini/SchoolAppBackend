91254486e4f8743c07b5b5629431a645342c03bc
GANGADHAR992 <gangadhar.ignitetech@gmail.com>
2023-10-17 16:03:32 +0530

    added entities
    added entities

--- src/main/java/net/javaguides/springboot/model/Fees.java
+++ src/main/java/net/javaguides/springboot/model/Fees.java
@@ -1,76 +1,26 @@
 package net.javaguides.springboot.model;
 
-import java.lang.annotation.Repeatable;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
-@Table(name="Fees")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection="fees")
 public class Fees {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
 	
-	@Column(name = "name")
+	private long _id;
 	private String name;
-
-	
-	@Column(name = "class")
-	private int classNumber;
-	
-	@Column(name = "fees")
+	private int standard;
 	private int fees;
 
-	public Fees() {
-		
-	}
-
-	public Fees(long id, String name, int classNumber, int fees) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.classNumber = classNumber;
-		this.fees = fees;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-
-	public int getClassNumber() {
-		return classNumber;
-	}
-
-	public void setClassNumber(int classNumber) {
-		this.classNumber = classNumber;
-	}
-
-	public int getFees() {
-		return fees;
-	}
-
-	public void setFees(int fees) {
-		this.fees = fees;
-	}
 	
 	
 
